1568c07d6da2d46f311c8429a6d13b1cad208c4c
j11042004 <j11042004@gmail.com>
2018-07-10 12:24:23 +0800

    新增說明

--- HelloPublicGet/ViewController.swift
+++ HelloPublicGet/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Uran on 2018/7/5.
 //  Copyright © 2018年 Uran. All rights reserved.
-//
+//  
 
 import UIKit
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Swift-OnlyPublicGet
+
+讓 Class 中的變數在 Public 只能被 Get ，只有在 Private 時才會被Set
